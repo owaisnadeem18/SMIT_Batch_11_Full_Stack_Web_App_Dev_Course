6ced474bb7d67b781e0e0913f75d4ccf17d15e69
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-08-09 16:02:26 +0500

    Add files via upload

---
+++ Mini Hackathon Preparation ( All JavaScript Revision )/All MCQ's Base Questions/MCQ's_prep.js
@@ -0,0 +1,124 @@
+// let x = 5;
+// let y = x++;
+// console.log(y);
+// console.log(x);
+
+// // Give some most important MCQ's preparation questions in JavaScript
+
+// // 1. What is the output of the following code?
+// let a = (b = 5);
+// console.log(b, "b");
+// console.log(a, "a");
+
+// // 2. What is the output of the following code?
+
+// console.log(program);
+
+// ----------------------------------
+
+// let x = 10;
+
+// function test() {
+//   console.log(x);
+//   x = 20;
+//   console.log(x);
+// }
+
+// test();
+
+// const person = { name: "John" };
+// Object.freeze(person);
+// person.name = "Doe";
+// console.log(person.name);
+
+// let a = [1, 2, 3, 4];
+// let b = a;
+// b.push(5);
+
+// console.log(a);
+
+// console.log(typeof null); //object
+
+// What will be output:
+
+// const numbers = [1, 2, 3];
+// numbers[10] = 11;
+// console.log(numbers.length);
+
+// let arr = [12, 4, 1, 3, 13, 4, 3];
+
+// arr = [];
+
+// arr.push(34);
+
+// console.log(arr);
+
+// console.log(this);
+
+// arr = [43, 19, 3, 9, 4, 2, 1];
+
+// let ans = arr.shift();
+
+// console.log(arr);
+// console.log(ans);
+
+// let animals = ["dog", "cat", "mouse"];
+// let index = animals.indexOf("Cat");
+// console.log(index);
+
+// It will return 1 if the array elem is present , otherwise will return -1
+
+// Splice Method in JavaScript
+
+// let arr = [3, 5, 2, 1, 5, 422, 90, 100, 13];
+
+// // arr.splice(3, 5, "Owais Nadeem");
+// arr.splice(3, 4, "Owais Nadeem");
+
+// console.log(arr);
+
+// let fruits = ["apple", "banana", "mango"];
+// fruits.splice(1, 1, "grape");
+// console.log(fruits);
+
+// Map method
+
+// arr = [3, 2, 1, 14];
+
+// // ans = arr.map((i) => i * 2);
+
+// let ans = arr.forEach((i) => console.log(i * 2));
+
+// console.log(ans);
+
+// console.log(ans);
+// console.log(arr); // does not modify the original array
+
+// ----------------------------------------------------------\
+
+// let arr = [1, 2, 3];
+// let result = arr.includes(1);
+// console.log(result);
+
+// It returns true , if there is elem present in the array
+
+// ===============================
+
+// let arr = [1, 2, 3, 4];
+// let result = arr.splice(1, 2, 5, 6);
+// console.log(result);
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4];
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(arr[i]);
+//   console.log(`Index ${i}`);
+// }
+
+// console.log(arr[-1]);
+
+// let arr = ["apple", "banana", "cherry"];
+// console.log(arr.includes("cherry", -2  ));
+
+// console.log(arr[arr.length - 1]);
